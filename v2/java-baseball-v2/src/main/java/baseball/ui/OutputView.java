eeefe57bc3a7d029b6a0afc15a2b73d174397865
leesky <leehaneul0623@gmail.com>
2022-12-05 12:23:22 +0900

    feat(controller, ui) : 게임 시작 로직 구현

--- v2/java-baseball-v2/src/main/java/baseball/ui/OutputView.java
+++ v2/java-baseball-v2/src/main/java/baseball/ui/OutputView.java
@@ -1,7 +1,11 @@
 package baseball.ui;
 
 public class OutputView {
+
+    public static final String GREET_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
     public void printGreet() {
+        System.out.println(GREET_MESSAGE);
     }
 
     public void printResult(int correctCount) {
